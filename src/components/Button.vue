c2762ade2efd60c14895c02444c943e3b543b005
brandondev <brandondev@brandondevs-Mac-mini.local>
2020-04-11 15:40:54 -0400

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-btn @click=consoleIt>Click Me</v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    methods: {
+      consoleIt() {
+        console.log('Just a test')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-text-field
+        placeholder="Testing">
+      </v-text-field>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
